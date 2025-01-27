6af1f7309c677d2c93d70b75f9d73e0318ee8bac
Ridhima <22b01a12b6@svecw.edu.in>
2025-01-27 18:24:30 +0530

    added routes

---
+++ src/Body.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Nabar";
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Body;

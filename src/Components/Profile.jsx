46c4c5488612c0bb02ceced6598f10d13cf67f06
Ridhima <22b01a12b6@svecw.edu.in>
2025-02-03 13:36:54 +0530

    feat:update profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import UpdateProfile from './UpdateProfile';
 
 const Profile = () => {
+   const user = useSelector((store) => store.User);
   return (
-    <div>Profile</div>
-  )
+    user && (<div>
+     <UpdateProfile user={user}/>
+    </div>)
+  );
 }
 
 export default Profile
---
+++ src/Components/UpdateProfile.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import UserCards from './UserCards';
+
+const UpdateProfile = ({user}) => {
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [age, setAge] = useState(user.age);
+    const [gender, setGender] = useState(user.gender);
+    const [about, setAbout] = useState(user.about);
+    const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+    const [skills, setSkills] = useState(user.skills);
+    const dispatch = useDispatch();
+  return (
+    <div>
+      <div className=" flex items-center justify-center mt-10">
+        <div className="card card-border bg-base-300  shadow-md">
+          <div className="card-body">
+            <h2 className="card-title justify-center text-2xl">
+              Update Profile
+            </h2>
+            <div className="flex gap-x-4">
+              <div>
+                <label className=" text-md font-bold ">FirstName</label>
+                <input
+                  className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label className=" text-md font-bold ">LastName</label>
+                <input
+                  className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div className="flex gap-x-4 mt-1">
+              <div>
+                <label className=" text-md font-bold">Age</label>
+                <input
+                  className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                ></input>
+              </div>
+              <div>
+                <label className=" text-md font-bold w-full">Gender</label>
+                <input
+                  className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                ></input>
+              </div>
+            </div>
+            <div>
+              <label className=" text-md font-bold w-full">About</label>
+              <textarea
+                className="w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+              ></textarea>
+            </div>
+            <div>
+              <label className=" text-md font-bold w-full">PhotoUrl</label>
+              <input
+                className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                value={photoUrl}
+                onChange={(e) => setPhotoUrl(e.target.value)}
+              ></input>
+            </div>
+            <div>
+              <label className=" text-md font-bold w-full">Skills</label>
+              <input
+                className=" w-full p-2.5 mt-1 border-black border shadow-md rounded-md"
+                value={skills}
+                onChange={(e) => setSkills(e.target.value)}
+              ></input>
+            </div>
+            <div className="card-actions justify-center mt-2.5">
+              <button className="btn btn-primary">Update</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateProfile
